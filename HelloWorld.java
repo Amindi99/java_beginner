0486365b2dded16aec17041cda7164a3b05f4882
Hashini_M <55966119+HashiniMudannayake@users.noreply.github.com>
2020-10-31 12:06:18 +0530

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,11 @@ package basic;
 
 public class HelloWorld {
 
+	//main method
 	public static void main(String[] args) {
 		
-		System.out.print("Hello, World!")
+		//print a simple statement
+		System.out.println("Hello, World!");
 
 	}
 
